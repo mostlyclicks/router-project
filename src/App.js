a7f0b6e9aef90f7b1d1752824000e83568f11014
Carlos Interone <carlos@mostlyclicks.com>
2021-09-14 20:58:47 -0600

    add basic routes for quotes/ new quote

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { Route, Switch, NavLink } from 'react-router-dom'
+import Quotes from './pages/Quotes'
+import NewQuote from './pages/NewQuote'
+
 function App() {
   return (
     <div>
-
+      <header>
+        <h1>Great Quotes</h1>
+        <nav>
+          <ul>
+            <li><NavLink to="/quotes">All Quotes</NavLink></li>
+          </ul>
+          <ul>
+          <li><NavLink to="/new-quote">Add a Quote</NavLink></li>
+          </ul>
+          
+        </nav>
+      </header>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
+      <Route path="/quotes">
+        <Quotes />
+      </Route>
     </div>
   );
 }
---
+++ src/pages/NewQuote.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewQuote = () => {
+  return (
+    <div>
+      <h1>Add a Quote</h1>
+    </div>
+  )
+}
+
+export default NewQuote
