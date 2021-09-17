9915ac377af42537142147b0729f25138d9c27d4
Carlos Interone <carlos@mostlyclicks.com>
2021-09-17 07:37:07 -0600

    add quote form

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import QuoteForm from '../components/quotes/QuoteForm'
 
 const NewQuote = () => {
+
+  const addQuoteHandler = quoteData => {
+    console.log(quoteData)
+  }
+
   return (
     <div>
       <h1>Add a Quote</h1>
+      <QuoteForm onAddQuote={addQuoteHandler} />
     </div>
   )
 }
