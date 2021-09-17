ac00638234f510d4d8cf0d8b19120b1b5abbb968
Carlos Interone <carlos@mostlyclicks.com>
2021-09-17 07:39:25 -0600

    add link to quoteItem

--- src/components/quotes/QuoteItem.js
+++ src/components/quotes/QuoteItem.js
@@ -1,6 +1,10 @@
 import classes from './QuoteItem.module.css';
+import { Route, Link } from 'react-router-dom'
 
 const QuoteItem = (props) => {
+
+  
+
   return (
     <li className={classes.item}>
       <figure>
@@ -9,9 +13,9 @@ const QuoteItem = (props) => {
         </blockquote>
         <figcaption>{props.author}</figcaption>
       </figure>
-      <a className='btn'>
+      <Link to={`/quotes/${props.id}`} className='btn'>
         View Fullscreen
-      </a>
+      </Link>
     </li>
   );
 };
