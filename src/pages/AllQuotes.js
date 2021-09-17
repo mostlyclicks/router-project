938763a1ca0b18f5faf25eb3ef8b73c1381a9569
Carlos Interone <carlos@mostlyclicks.com>
2021-09-17 07:25:11 -0600

    add dummy_quotes

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import QuoteList from '../components/quotes/QuoteList'
+
+const DUMMY_QUOTES = [
+  {id: 1, author: 'Max', text: 'Learning react is fun'},
+  {id: 2, author: 'Carlos', text: 'Learning react is great'}
+]
 
 const AllQuotes = () => {
   return (
     <div>
-      <h1>Quotes</h1>
+      <h1>All Quotes</h1>
+      <QuoteList quotes={DUMMY_QUOTES} />
     </div>
   )
 }
