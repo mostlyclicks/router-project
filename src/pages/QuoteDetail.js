209e188ee0c359798c3baea9179984af4463b4d4
Carlos Interone <carlos@mostlyclicks.com>
2021-09-23 20:05:45 -0600

    add close button

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -23,10 +23,14 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author}/>
-      <Link to={`/quotes/${params.quoteId}/comments`}>See Comments</Link>
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className='centered'><Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>Load Comments</Link></div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
+        <Link to={`/quotes/${params.quoteId}`} className="centered">Hide Comments</Link>
       </Route>
+
     </Fragment>
   )
 }
