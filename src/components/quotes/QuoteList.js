59bfbcab1bbbcb14116447f4c1b89f7a1c192338
Carlos Interone <carlos@mostlyclicks.com>
2021-09-24 20:53:11 -0600

    refactor sort query using history.push

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -27,7 +27,11 @@ const QuoteList = (props) => {
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending)
 
   const changeSortingHandler = () => {
-    history.push(`${location.pathname}?sort=${(isSortingAscending ? 'desc' : 'asc')}`)
+    history.push({
+      pathname: location.pathname,
+      search: `?sort=${(isSortingAscending ? 'desc' : 'asc')}`
+    })
+    // history.push(`${location.pathname}?sort=${(isSortingAscending ? 'desc' : 'asc')}`)
   }
 
 
